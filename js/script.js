f523a9d0fdee69d321236fece815dd561e9d1ac7
Kipkorir2017 <pkobenj41@gmail.com>
2021-07-13 15:42:47 +0300

    added all arithmetic operations

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
+// Addition
 var add = function(number1, number2) {
     return number1 + number2;
   };
-  var number1 = parseInt(prompt("Enter a number:"));
-  var number2 = parseInt(prompt("Enter another number:"));
+  var number1 = parseInt(prompt("Addition-"+"Enter a number:"));
+  var number2 = parseInt(prompt("Addition-"+"Enter another number:"));
   alert(add(number1, number2));
+//   Subtraction
+var sub =function(number1,number2){
+    return number1 - number2;
+}
+var number1 =parseInt(prompt("Sub-"+"Enter a number:"));
+var number2 =parseInt(prompt("Sub-"+"Enter another number:"))
+var result = sub(number1,number2);
+alert(result);
+// multiplication
+var sub =function(number1,number2){
+    return number1 * number2;
+}
+var number1 =parseInt(prompt("Multiply-"+"Enter a number:"));
+var number2 =parseInt(prompt("Multiply-"+"Enter another number:"))
+var result = sub(number1,number2);
+alert(result);
+// Division
+var sub =function(number1,number2){
+    return number1 / number2;
+}
+var number1 =parseInt(prompt("Division-"+"Enter a number:"));
+var number2 =parseInt(prompt("Division-"+"Enter another number:"))
+var result = sub(number1,number2);
+alert(result);
